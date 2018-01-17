474d1f6b5917c9ce1cdae89772549f4419fc1e11
crypto <webdohod781@gmail.com>
2018-01-17 21:12:00 +0200

    Add files via upload

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "http://your pool name:port";
+
+var poolHost = "your pool name";
+
+var irc = "irc.freenode.net/#Ytransfer";
+
+var email = "support@your pool name.org";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://ytansfer.online/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://ytransfer.online/?hash={id}#blockchain_transaction";
+
+var themeCss = "default-theme.css";
